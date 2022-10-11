b0f95614186d463bf96875bec6d3d25bf9f3136d
Nikita <nikki-xit@mail.ru>
2022-10-11 09:10:07 +0300

    update pages styles

--- urgentPhotoForDocument.php
+++ urgentPhotoForDocument.php
@@ -10,13 +10,11 @@ Template Name: urgentPhotoForDocument
     <section class="mainScreen">
         <div class="container">
             <div class="mainScreenWrapper">
-                <a class="logo" href="/">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/images/logo/logo.png" alt="logo">
-                </a>
+                <a class="logo" href="/"></a>
                 <div class="mainScreenContent">
                     <div class="mainScreenContent--TitleWrapper">
-                        <h1 class="mainScreenContant--Title">Фото на документы</h1>
-                        <h1 class="mainScreenContant--Titlesub">срочно c профессиональным светом ProFoto и красивой ретушью рядом в Москве и онлайн</h1>
+                        <h1 class="mainScreenContent--Title">Фото на документы</h1>
+                        <h1 class="mainScreenContent--TitleSub">срочно c профессиональным светом ProFoto и красивой ретушью рядом в Москве и онлайн</h1>
                     </div>
                     <div class="linkToGoBlock">
                         <a href="/" class="linkToGo">
@@ -26,6 +24,257 @@ Template Name: urgentPhotoForDocument
                             <h3>Срочное фото на документы</h3>
                         </a>
                     </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="linkNavigate">
+        <div class="container">
+            <div class="linkNavigateWrapper">
+                <ul class="linkNavigateList">
+                    <li class="linkNavigateItem">
+                        <h2 class="linkNavigateItem--title">
+                            <a href="#holliwood_light">Голливудский студийный свет</a>
+                        </h2>
+                    </li>
+                    <li class="linkNavigateItem">
+                        <h2 class="linkNavigateItem--title">
+                            <a href="#holliwood_light">Художественная обработка</a>
+                        </h2>
+                    </li>
+                    <li class="linkNavigateItem">
+                        <h2 class="linkNavigateItem--title">
+                            <a href="#holliwood_light">Действительно очень срочно</a>
+                        </h2>
+                    </li>
+                    <li class="linkNavigateItem">
+                        <h2 class="linkNavigateItem--title">
+                            <a href="#holliwood_light">Рядом с метро и Вами в Москве</a>
+                        </h2>
+                    </li>
+                    <li class="linkNavigateItem">
+                        <h2 class="linkNavigateItem--title">
+                            <a href="#holliwood_light">Онлайн срочное фото с доставкой</a>
+                        </h2>
+                    </li>
+                </ul>
+
+                <a class="btn  linkNavigateBtn">
+                    <h3>Цены</h3>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section class="bigPhoto">
+        <div class="container">
+            <div class="bigPhotoWrapper">
+                <div class="bigPhotoWrapperAbs">
+                    <div class="bigPhotoBlock">
+                        <h3 class="bigPhotoBlock--title">По отзывам от тех, кому не всё равно!</h3>
+                    </div>
+                    <a class="btn bigPhotoBtn">Ваши отзывы о проделанной работе</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sliderPortrait">
+        <div class="container">
+            <div class="sliderPortraitWrapper">
+                <h2 class="sliderPortraitTitle">
+                    ВРЕМЯ & ДЕНЬГИ
+                </h2>
+
+                <div class="sliderPortraitBlock">
+                    <p class="sliderPortraitBlock--title">
+                        Не тратим на <span>Make-Up</span>
+                        <br>Включена журнальная
+                        <br>обработка в стоимость
+                    </p>
+
+                    <div class="sliderPortraitLists">
+                        <ul class="sliderPortraitList">
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/image/sliderPortrait/sliderPortraitItem1.jpg" alt="© Деловое Фото.">
+                            </li>
+                            <!-- <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li>
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li>
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li> -->
+                        </ul>
+
+                        <div class="sliderPortraitItemFix">
+                            <h3 class="sliderPortraitItemFix--titleSub">
+                                Без записи
+                            </h3>
+                            <h3 class="sliderPortraitItemFix--title">
+                                <span>500</span> руб / Компл.
+                                <br>Красивое фото
+                            </h3>
+                            <h3 class="sliderPortraitItemFix--text">
+                                На любые документы*
+                            </h3>
+
+                            <ul class="СharacteristicsList">
+                                <li class="СharacteristicsItem">
+                                    <span>4 минуты съёмки</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>2 кадра на выбор</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>1 классический свет</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>18 Mega Pixels Camera</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="sliderPortraitLists">
+                        <ul class="sliderPortraitList">
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/image/sliderPortrait/sliderPortraitItem2.jpg" alt="© Деловое Фото.">
+                            </li>
+                            <!-- <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li>
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li>
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li> -->
+                        </ul>
+
+                        <div class="sliderPortraitItemFix">
+                            <h3 class="sliderPortraitItemFix--titleSub">
+                                По записи и без
+                            </h3>
+                            <h3 class="sliderPortraitItemFix--title">
+                                <span>2000 </span> руб / Компл.
+                                <br>Топ свет, камера
+                            </h3>
+                            <h3 class="sliderPortraitItemFix--text">
+                                Студийный портрет на документы*
+                            </h3>
+
+                            <ul class="СharacteristicsList">
+                                <li class="СharacteristicsItem">
+                                    <span>10 мин съёмки</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>4-8 кадра на выбор</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>2 Стрипа ProFoto</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>38 Mega Pixels Pro Camera</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="sliderPortraitLists">
+                        <ul class="sliderPortraitList">
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/image/sliderPortrait/sliderPortraitItem3.jpg" alt="© Деловое Фото.">
+                            </li>
+                            <!-- <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li>
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li>
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li> -->
+                        </ul>
+
+                        <div class="sliderPortraitItemFix">
+                            <h3 class="sliderPortraitItemFix--titleSub">
+                                Онлайн фото
+                            </h3>
+                            <h3 class="sliderPortraitItemFix--title">
+                                <span>700</span> руб / компл.
+                                <br>Онлайн + доставка
+                            </h3>
+                            <h3 class="sliderPortraitItemFix--text">
+                                Не выходя из дома на свой смартфон*
+                            </h3>
+
+                            <ul class="СharacteristicsList">
+                                <li class="СharacteristicsItem">
+                                    <span>Инструкция по самостоятельной съёмке</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>Выбор лучшей фото в обработку</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>Гарантия совместимости</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>Доставка до двери</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="sliderPortraitLists">
+                        <ul class="sliderPortraitList">
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/image/sliderPortrait/sliderPortraitItem4.jpg" alt="© Деловое Фото.">
+                            </li>
+                            <!-- <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li>
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li>
+                            <li class="sliderPortraitItem">
+                                <img class="sliderPortraitItem--img" src="<?php bloginfo('template_url'); ?>/assets/images/" alt="© Деловое Фото.">
+                            </li> -->
+                        </ul>
+
+                        <div class="sliderPortraitItemFix">
+                            <h3 class="sliderPortraitItemFix--titleSub">
+                                НОВИНКА!
+
+                            </h3>
+                            <h3 class="sliderPortraitItemFix--title">
+                                <span>300</span> руб / Компл.
+                                <br>Бюджетный
+                            </h3>
+                            <h3 class="sliderPortraitItemFix--text">
+                                Без настроек, подсказок и обработки*
+                            </h3>
+
+                            <ul class="СharacteristicsList">
+                                <li class="СharacteristicsItem">
+                                    <span>4 минуты съёмки</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>2 кадра на выбор</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>1 классический свет</span>
+                                </li>
+                                <li class="СharacteristicsItem">
+                                    <span>18 Mega Pixels Camera</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
